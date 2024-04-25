abd2712a522854fc715c756f62d3d63cee2e46ff
hannahschwen <hannahschwen@gmail.com>
2024-04-25 16:19:11 -0400

    fixed img path on slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -3,16 +3,16 @@ const images = [];
 const time = 4000;  // time between slides
 
 // image list
-images[0] = '../assets/images/1.jpg';
-images[1] = '../assets/images/3.jpg';
-images[2] = '../assets/images/5.jpg';
-images[3] = '../assets/images/7.jpg';
-images[4] = '../assets/images/9.jpg';
-images[5] = '../assets/images/11.jpg';
-images[6] = '../assets/images/27.jpg';
-images[7] = '../assets/images/29.jpg';
-images[8] = '../assets/images/33.jpg';
-images[9] = '../assets/images/35.jpg';
+images[0] = '././assets/images/1.jpg';
+images[1] = '././assets/images/3.jpg';
+images[2] = '././assets/images/5.jpg';
+images[3] = '././assets/images/7.jpg';
+images[4] = '././assets/images/9.jpg';
+images[5] = '././assets/images/11.jpg';
+images[6] = '././assets/images/27.jpg';
+images[7] = '././assets/images/29.jpg';
+images[8] = '././assets/images/33.jpg';
+images[9] = '././assets/images/35.jpg';
 
 // change image
 function changeImg() {
